346131e115eb665c2e29f9763b8d31fecac97073
lebedevkirill <lebedevkirill@LEBEDEVKIRI0550>
2022-05-26 12:46:49 +0600

    Добавьте файлы проекта.

---
+++ TEXT_ANALIZATOR/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TEXT_ANALIZATOR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(353, 228);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(-1, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 627);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "++++++++";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(64, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(353, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Посчитать слова";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(64, 279);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(353, 348);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(491, 0);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(164, 627);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(415, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 627);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Сохранить ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1216, 626);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TEXT_ANALIZATOR/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TEXT_ANALIZATOR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private Dictionary<string, int> frequency = new Dictionary<string, int>();
+        private int count = 0;
+
+        public Dictionary<string, int> Method(string[] masSt)
+        {
+            textBox2.Text = string.Empty;
+            Dictionary<string, int> FreqDic = new Dictionary<string, int>();
+            foreach (string s in masSt)
+                if (FreqDic.ContainsKey(s.ToUpper()))
+                    FreqDic[s.ToUpper()]++;
+                else
+                    FreqDic.Add(s.ToUpper(), 1);
+            var stringBuilder = new StringBuilder();
+            foreach (var k_v in FreqDic.OrderByDescending(x => x.Value))
+            {
+                stringBuilder.Append(k_v.Key);
+                stringBuilder.Append(" ");
+                stringBuilder.Append(k_v.Value);
+                stringBuilder.Append(Environment.NewLine);
+            }
+            textBox2.Text = stringBuilder.ToString();
+            return FreqDic;
+
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string inpf;
+                OpenFileDialog od = new OpenFileDialog();
+                od.Filter = "Текстовые файлы|*.txt";
+                od.Title = "Выберите файл с текстом для анализа";
+                if (od.ShowDialog() == DialogResult.OK)
+                {
+                    inpf = od.FileName;
+                    SaveFileDialog sv = new SaveFileDialog();
+                    sv.Filter = "Текстовые файлы|*.txt";
+                    sv.Title = "Выберите файл для сохранения результата";
+                    if (sv.ShowDialog() == DialogResult.OK)
+                    {
+                        od.Title = "Выберите файл программы mystem";
+                        od.Filter = "Приложения|*.exe";
+                        if (od.ShowDialog() == DialogResult.OK)
+                        {
+                            System.Diagnostics.Process command = new System.Diagnostics.Process();
+                            command.StartInfo.FileName = od.FileName;
+                            command.StartInfo.Arguments = inpf + " " + sv.FileName;
+                            command.Start();
+                            MessageBox.Show("Успешно!!");
+                            textBox1.Text = File.ReadAllText(sv.FileName);
+                            textBox2.Text = string.Empty;
+                            string[] Mas = textBox1.Text.Split(new char[] { '}' }, StringSplitOptions.RemoveEmptyEntries);
+                            var ItogS = new StringBuilder();
+                            var textBox2Text = new StringBuilder();
+
+                            foreach (string s in Mas)
+                            {
+                                if (s.Contains('|'))
+                                {
+                                    textBox2Text.Append(s.Remove(s.IndexOf('|')));
+                                    textBox2Text.Append(Environment.NewLine);
+                                }
+                                else
+                                {
+                                    textBox2Text.Append(s);
+                                    textBox2Text.Append(Environment.NewLine);
+                                }
+                                string[] st = s.Split('{');
+                                if (st[1].Contains('|'))
+                                {
+                                    ItogS.Append(st[1].Remove(st[1].IndexOf('|')));//
+                                    ItogS.Append(" ");
+                                }
+                                else
+                                {
+                                    ItogS.Append(st[1]);
+                                    ItogS.Append(" ");
+                                }
+                            }
+                            textBox1.Text = ItogS.ToString();
+                            textBox2.Text = textBox2Text.ToString();
+                        }
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] masST = textBox1.Text.ToString().Split(new char[] { ' ', ',', '.', '!', '?', '-', ':' }, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> FreqDic = Method(masST);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
+
